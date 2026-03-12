d01876f39c7ad4dc966e21ba8b249f8180ab9a4c
Daniel <danielqian64@gmail.com>
2026-03-13 02:58:37 +0800

    Fix delete parser bug

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -9,31 +12,40 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new DeleteCommand object
  */
-public DeleteCommand parse(String args) throws ParseException {
-    String trimmed = args.trim();
-    if (trimmed.isEmpty()) {
-        throw new ParseException(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-    }
+public class DeleteCommandParser implements Parser<DeleteCommand> {
 
-    String[] indexTokens = trimmed.split("\\s+");
-    List<Index> indexes = new ArrayList<>();
-
-    for (String token : indexTokens) {
-        try {
-            indexes.add(ParserUtil.parseIndex(token));
-        } catch (ParseException pe) {
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * and returns a DeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public DeleteCommand parse(String args) throws ParseException {
+        String trimmed = args.trim();
+        if (trimmed.isEmpty()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
+
+        String[] indexTokens = trimmed.split("\\s+");
+        List<Index> indexes = new ArrayList<>();
+
+        for (String token : indexTokens) {
+            try {
+                indexes.add(ParserUtil.parseIndex(token));
+            } catch (ParseException pe) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+            }
+        }
+
+        // Reject duplicate indices
+        long uniqueCount = indexes.stream().map(Index::getZeroBased).distinct().count();
+        if (uniqueCount != indexes.size()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
+
+        return new DeleteCommand(indexes);
     }
 
-    // Reject duplicate indices
-    long uniqueCount = indexes.stream().map(Index::getZeroBased).distinct().count();
-    if (uniqueCount != indexes.size()) {
-        throw new ParseException(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-    }
-
-    return new DeleteCommand(indexes);
 }
